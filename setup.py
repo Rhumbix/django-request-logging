acc6a5a83fc454fac59acf299679f0c69694073a
Kenneth Jiang <kenneth@rhumbix.com>
2015-08-14 01:31:29 +0000

    Rename package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='django-request-logging',
       version='0.1',
-      description='Django middleware that logs http request body.'
+      description='Django middleware that logs http request body.',
       url='https://github.com/rhumbixsf/django-request-logging.git',
       author='Kenneth Jiang',
       author_email='kenneth@rhumbix.com',
