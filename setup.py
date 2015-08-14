b962d91136aacf1a7815e33ff8fdbdf7398bdeb4
Kenneth Jiang <kenneth@rhumbix.com>
2015-08-14 01:04:19 +0000

    Bare minimum

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='django-request-logging',
+      version='0.1',
+      description='Django middleware that logs http request body.'
+      url='https://github.com/rhumbixsf/django-request-logging.git',
+      author='Kenneth Jiang',
+      author_email='kenneth@rhumbix.com',
+      license='MIT',
+      packages=['django-request-logging'],
+      zip_safe=False)
