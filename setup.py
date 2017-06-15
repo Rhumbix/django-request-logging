2de0122c34816062cd5e3d37e127aba63756afc2
Kenneth Jiang <kenneth.jiang@gmail.com>
2017-06-15 10:57:48 -0700

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='django-request-logging',
-      version='0.4.6',
+      version='0.5.0',
       description='Django middleware that logs http request body.',
       url='https://github.com/Rhumbix/django-request-logging.git',
       author='Kenneth Jiang',
