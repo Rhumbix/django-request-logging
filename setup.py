b557e3e8c12bce4862a59fe7598fae8a28a9b3ff
Steven Lee <steven.lee@qadium.com>
2017-10-18 13:22:34 -0700

    bumped version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='django-request-logging',
-      version='0.5.2',
+      version='0.5.3',
       description='Django middleware that logs http request body.',
       url='https://github.com/Rhumbix/django-request-logging.git',
       author='Kenneth Jiang',
