71af2bbf84cfbecf808a877e0a0804e87f2efac5
Calvin Wu <calvin620707@msn.com>
2017-11-03 18:36:47 +0800

    skip new test case in python2
    because this issue won't happen in python 2

--- tests.py
+++ tests.py
@@ -5,6 +5,7 @@ import re
 
 import logging
 import mock
+import sys
 from django.conf import settings
 from django.test import RequestFactory, override_settings
 
@@ -65,6 +66,7 @@ class LogTestCase(BaseLogTestCase):
         self.middleware.process_request(request)
         self._assert_logged(mock_log, "(binary data)")
 
+    @unittest.skipIf(sys.version_info < (3, 0), "This issue won't happen on python 2")
     def test_request_jpeg_logged(self, mock_log):
         body = b'--BoUnDaRyStRiNg\r\nContent-Disposition: form-data; name="file"; filename="campaign_carousel_img.jp' \
                b'g"\r\nContent-Type: image/jpeg\r\n\r\n\xff\xd8\xff\xe1\x00\x18Exif\x00\x00II*\x00\x08\x00\x00\x00' \
@@ -108,6 +110,7 @@ class LogTestCase(BaseLogTestCase):
         self._assert_logged(mock_log, "test_headers")
         self._assert_logged(mock_log, "HTTP_USER_AGENT")
 
+    @unittest.skipIf(sys.version_info < (3, 0), "This issue won't happen on python 2")
     def test_call_jpeg_logged(self, mock_log):
         body = b'--BoUnDaRyStRiNg\r\nContent-Disposition: form-data; name="file"; filename="campaign_carousel_img.jp' \
                b'g"\r\nContent-Type: image/jpeg\r\n\r\n\xff\xd8\xff\xe1\x00\x18Exif\x00\x00II*\x00\x08\x00\x00\x00' \
