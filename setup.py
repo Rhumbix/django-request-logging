35ac2254d811fc7584216505ef57756856f68b8e
Mark Eschbach <mark@rhumbix.com>
2018-04-30 17:47:46 -0700

    [releng/manifest] Includes missing files

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='django-request-logging',
-      version='0.6.1',
+      version='0.6.2',
       description='Django middleware that logs http request body.',
       url='https://github.com/Rhumbix/django-request-logging.git',
       author='Rhumbix',
