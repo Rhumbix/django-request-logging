c31fe5e0c82633268201aa8426ad94d9e38243bb
Mark Eschbach <mark@rhumbix.com>
2018-05-02 10:27:20 -0700

    [decorators] Removes no-op wrapper on no_logging
    Since the wrapper didn't perform any particular function this avoids an
    unnecessary wrapper.

--- request_logging/decorators.py
+++ request_logging/decorators.py
@@ -5,9 +5,5 @@ from .middleware import NO_LOGGING_ATTR, NO_LOGGING_MSG
 def no_logging(msg=None):
     def wrapper(func):
         setattr(func, NO_LOGGING_ATTR, msg if msg else NO_LOGGING_MSG)
-
-        @wraps(func)
-        def decorator(*args, **kwargs):
-            return func(*args, **kwargs)
-        return decorator
+        return func
     return wrapper
