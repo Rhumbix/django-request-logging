f36992199c2bba5419b3dd6fa9c5942b94a3fcfe
Tom Clancy <tclancy@gmail.com>
2018-05-04 11:09:44 -0400

    Respect no_logging in response, add changelog, bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,5 +3,5 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 
-## 0.6.2
+## 0.6.3
 - Respect `@no_logging` decorator in request and response logging
--- request_logging/middleware.py
+++ request_logging/middleware.py
@@ -3,20 +3,14 @@ import re
 
 from django.conf import settings
 from django.utils.termcolors import colorize
-try:
-    # Django 1.x
-    from django.core.urlresolvers import resolve
-except ImportError:
-    # Django 2.x
-    from django.urls import resolve
+from django.core.urlresolvers import resolve
 
 DEFAULT_LOG_LEVEL = logging.DEBUG
 DEFAULT_COLORIZE = True
 DEFAULT_MAX_BODY_LENGTH = 50000  # log no more than 3k bytes of content
 SETTING_NAMES = {
     'log_level': 'REQUEST_LOGGING_DATA_LOG_LEVEL',
-    'legacy_colorize': 'REQUEST_LOGGING_DISABLE_COLORIZE',
-    'colorize': 'REQUEST_LOGGING_ENABLE_COLORIZE',
+    'colorize': 'REQUEST_LOGGING_DISABLE_COLORIZE',
     'max_body_length': 'REQUEST_LOGGING_MAX_BODY_LENGTH'
 }
 BINARY_REGEX = re.compile(r'(.+Content-Type:.*?)(\S+)/(\S+)(?:\r\n)*(.+)', re.S | re.I)
@@ -61,17 +55,14 @@ class ColourLogger(Logger):
 class LoggingMiddleware(object):
     def __init__(self, get_response=None):
         self.get_response = get_response
+        self.no_logging_reason = None
 
         self.log_level = getattr(settings, SETTING_NAMES['log_level'], DEFAULT_LOG_LEVEL)
         if self.log_level not in [logging.NOTSET, logging.DEBUG, logging.INFO,
                                   logging.WARNING, logging.ERROR, logging.CRITICAL]:
             raise ValueError("Unknown log level({}) in setting({})".format(self.log_level, SETTING_NAMES['log_level']))
 
-        # TODO: remove deprecated legacy settings
-        enable_colorize = getattr(settings, SETTING_NAMES['legacy_colorize'], None)
-        if enable_colorize is None:
-            enable_colorize = getattr(settings, SETTING_NAMES['colorize'], DEFAULT_COLORIZE)
-
+        enable_colorize = getattr(settings, SETTING_NAMES['colorize'], DEFAULT_COLORIZE)
         if not isinstance(enable_colorize, bool):
             raise ValueError(
                 "{} should be boolean. {} is not boolean.".format(SETTING_NAMES['colorize'], enable_colorize)
@@ -87,77 +78,51 @@ class LoggingMiddleware(object):
         self.boundary = ''
 
     def __call__(self, request):
-        self.process_request( request )
-        response = self.get_response( request )
-        self.process_response( request, response )
+        self.process_request(request)
+        response = self.get_response(request)
+        self.process_response(request, response)
         return response
 
     def process_request(self, request):
-        skip_logging_because = self._should_log_route(request)
-        if skip_logging_because:
-            return self._skip_logging_request(request, skip_logging_because)
-        else:
-            return self._log_request(request)
-
-    def _should_log_route(self, request):
-        try:
-            route_match = resolve(request.path)
-        except:
-            return None
-
-        method = request.method.lower()
-        view = route_match.func
-        func = view
-        # This is for "django rest framework"
-        if hasattr(view, 'cls'):
-            if hasattr(view, 'actions'):
-                actions = view.actions
-                if hasattr(actions, method):
-                    func = getattr(view.cls, view.actions[method], None)
-            else:
-                func = getattr(view.cls, method, None)
-        elif hasattr(view, 'view_class'):
-            # This is for django class-based views
-            func = getattr(view.view_class, method, None)
-        no_logging = getattr(func, NO_LOGGING_ATTR, None)
-        return no_logging
-
-    def _skip_logging_request(self, request, reason):
         method_path = "{} {}".format(request.method, request.get_full_path())
-        no_log_context = {
-            'args': (),
-            'kwargs': {
-                'extra': {
-                    'no_logging': reason
-                },
-            },
-        }
-        self.logger.log(logging.INFO, method_path + " (not logged because '" + reason + "')", no_log_context)
-
-    def _log_request(self, request):
-        method_path = "{} {}".format(request.method, request.get_full_path())
-
         logging_context = self._get_logging_context(request, None)
         self.logger.log(logging.INFO, method_path, logging_context)
-        self._log_request_headers(request, logging_context)
-        self._log_request_body(request, logging_context)
 
-    def _log_request_headers(self, request, logging_context):
+        try:
+            route_match = resolve(request.path)
+            method = request.method.lower()
+            view = route_match.func
+            func = view
+            # This is for "django rest framework"
+            if hasattr(view, 'cls'):
+                if hasattr(view, 'actions'):
+                    func = getattr(view.cls, view.actions[method], None)
+                else:
+                    func = getattr(view.cls, method, None)
+            elif hasattr(view, 'view_class'):
+                # This is for django class-based views
+                func = getattr(view.view_class, method, None)
+            self.no_logging_reason = getattr(func, NO_LOGGING_ATTR, None)
+        except:
+            pass
+
+        content_type = request.META.get('CONTENT_TYPE', '')
+        is_multipart = content_type.startswith('multipart/form-data')
+        if is_multipart:
+            self.boundary = '--' + content_type[30:]  # First 30 characters are "multipart/form-data; boundary="
+
         headers = {k: v for k, v in request.META.items() if k.startswith('HTTP_')}
 
         if headers:
             self.logger.log(self.log_level, headers, logging_context)
-
-    def _log_request_body(self, request, logging_context):
         if request.body:
-            content_type = request.META.get('CONTENT_TYPE', '')
-            is_multipart = content_type.startswith('multipart/form-data')
-            if is_multipart:
-                self.boundary = '--' + content_type[30:]  # First 30 characters are "multipart/form-data; boundary="
-            if is_multipart:
-                self._log_multipart(self._chunked_to_max(request.body), logging_context)
+            if self.no_logging_reason is not None:
+                self.logger.log(self.log_level, self.no_logging_reason, logging_context)
             else:
-                self.logger.log(self.log_level, self._chunked_to_max(request.body), logging_context)
+                if is_multipart:
+                    self._log_multipart(self._chunked_to_max(request.body), logging_context)
+                else:
+                    self.logger.log(self.log_level, self._chunked_to_max(request.body), logging_context)
 
     def process_response(self, request, response):
         resp_log = "{} {} - {}".format(request.method, request.get_full_path(), response.status_code)
@@ -176,6 +141,7 @@ class LoggingMiddleware(object):
         """
         Returns a map with args and kwargs to provide additional context to calls to logging.log().
         This allows the logging context to be created per process request/response call.
+
         """
         return {
             'args': (),
@@ -216,7 +182,8 @@ class LoggingMiddleware(object):
     def _log_resp(self, level, response, logging_context):
         if re.match('^application/json', response.get('Content-Type', ''), re.I):
             self.logger.log(level, response._headers, logging_context)
-            self.logger.log(level, self._chunked_to_max(response.content), logging_context)
+            to_log = self.no_logging_reason if self.no_logging_reason else self._chunked_to_max(response.content)
+            self.logger.log(level, to_log, logging_context)
 
     def _chunked_to_max(self, msg):
         return msg[0:self.max_body_length]
