d1afd87455a0700898073783b622c700129402f7
Tom Clancy <tclancy@gmail.com>
2018-10-24 08:59:35 -0400

    Update version, provide description

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from setuptools import setup
 
-setup(name='django-request-logging',
-      version='0.6.5',
-      description='Django middleware that logs http request body.',
-      url='https://github.com/Rhumbix/django-request-logging.git',
-      author='Rhumbix',
-      author_email='devteam@rhumbix.com',
-      license='MIT',
-      packages=['request_logging'],
-      install_requires=[
-          'Django',
-      ],
-      zip_safe=False)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='django-request-logging',
+    version='0.6.6',
+    description='Django middleware that logs http request body.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Rhumbix/django-request-logging.git',
+    author='Rhumbix',
+    author_email='dev@rhumbix.com',
+    license='MIT',
+    packages=['request_logging'],
+    install_requires=[
+        'Django',
+    ],
+    zip_safe=False
+)
